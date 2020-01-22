c6304fa0ce312532e50386aa2786578181d9cdc2
JulienGrade <gradejulien@gmail.com>
2020-01-22 15:04:40 +0100

    create home page and implement geolocation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation }  from '@ionic-native/geolocation/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private geolocation: Geolocation,
+    private router: Router
+    ) {}
+  ngOnInit(){
+    // Au chargement de l'appli n affiche les coords gps
+    this.geolocation.watchPosition().subscribe((data)=>{
+      console.log(data);
+    });
+  }
 
+  // On se rend sur la page /about
+  navToAbout(){
+    this.router.navigate(['/about']);
+  }
 }
